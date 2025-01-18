c6a1dd0c4c6bfab3531c7b7f17d9d8a97426d961
wi113565 <86216@student.pb.edu.pl>
2025-01-18 19:07:14 +0100

    Dodano podstawowe łączenie frontendu z backendem, poprawnie wyświetla dane z backendu we frontendzie. Narazie w app jako przykład jak to robic

--- frontend2/src/App.tsx
+++ frontend2/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [items, setItems] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/item/')
+      .then(response => {
+        setItems(response.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError('Error fetching data');
+        setLoading(false);
+      });
+  }, []);
+
+
+  if (loading) return <div>Loading...</div>; 
+  if (error) return <div>{error}</div>; 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name}
+            <br></br>
+            {item.description}   
+            <br></br>
+            {item.weight}
+            <br></br>
+            {item.calories}
+            <br></br>
+            {item.carbohydrates}
+            <br></br>
+            {item.proteins}
+            <br></br>
+            {item.fats}       
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
