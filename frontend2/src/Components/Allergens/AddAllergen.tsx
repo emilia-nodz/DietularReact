ef01b0ce82e931ed4c618baac0ec52c5610c661d
emilia-nodz <extra.maslo.game@gmail.com>
2025-01-20 18:58:55 +0100

    Komponent reużywalny

--- frontend2/src/Components/Allergens/AddAllergen.tsx
+++ frontend2/src/Components/Allergens/AddAllergen.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import "../../Styles/Form.css"
 import {AllergenData, addAllergen } from "../../Services/AllergenService";
- 
+import { LightButton } from "../Button";
+
 const AddAllergen = () => {
     const [NewAllergenName, setAllergenName] = useState<string>('');
     const [allergens, setAllergens] = useState<AllergenData[]>([]); 
 
-  const HandlePost = async () => {
+    const HandlePost = async () => {
         if(NewAllergenName.length>0) {
             try {
                 const newAllergen = await addAllergen({name: NewAllergenName})
@@ -22,34 +23,29 @@ const AddAllergen = () => {
  
     const HandleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setAllergenName(event.target.value);
-      }
+    }
    
    
     return (
         <>
             <div className="main-container">
-            <h1>Add allergen</h1>
-            
-            <input 
-                name="NewAllergenNameInput" 
-                value={NewAllergenName} 
-                onChange={HandleInputChange} 
-            />
-             <button onClick={HandlePost}>Confirm</button>
-
-            {/* <div className="form-container">
-                <form [formGroup]="formModel" (ngSubmit)="submitForm()">
+                <h1>Add allergen</h1>
+                <div className="form-container">
                     <div className="form-thing">
                         <label>Name</label>
-                        <input type="text" formControlName="name" placeholder="Name">
-                        @if(formModel.controls['name'].errors?.['required']){
-                            <span className="error">Name is required</span>
-                        }
+                        <input 
+                            name="NewAllergenNameInput" 
+                            value={NewAllergenName} 
+                            onChange={HandleInputChange} 
+                        />
                     </div>
                     <div className="form-thing">
-                        <input [disabled]="!formModel.valid" type="submit" value="Add allergen" className="btn-view">
-                    </div>  
-                </form> */}
+                        <LightButton
+                            label="Confirm"
+                            onClick={HandlePost}
+                        />
+                    </div>
+                </div>
             </div>
         </>
     );
