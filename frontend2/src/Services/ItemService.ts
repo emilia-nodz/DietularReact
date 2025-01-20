7d4a035c5edba9f4c6240e9f229920258bc36969
TamaTamaTamaTamaTama <domizabiel12@gmail.com>
2025-01-20 23:09:40 +0100

    Wstępne zaimplementowanie getpost artykułów spożywczych

--- frontend2/src/Services/ItemService.ts
+++ frontend2/src/Services/ItemService.ts
@@ -1,1 +1,72 @@
-import React from "react";
+import AxiosInstance from "./Axios";
+import { AllergenData } from "./AllergenService";
+
+
+
+export interface NewItemData {
+    allergen_details: AllergenData[];
+    name: string;
+    description: string;
+    weight: number;
+    calories: number;
+    carbohydrates: number;
+    proteins: number;
+    fats: number;
+  }
+
+export interface ItemData {
+    allergen_details: AllergenData[];
+    id: number;
+    name: string;
+    description: string;
+    weight: number;
+    calories: number;
+    carbohydrates: number;
+    proteins: number;
+    fats: number;
+  }
+
+
+
+export const getItems = async(): Promise<ItemData[]> => {
+    try {
+        const response = await AxiosInstance.get('item/');
+        return response.data;
+        
+    } catch (error) {
+        console.error('Error fetching items:', error);
+        throw error;
+      }
+    };
+
+export const addItem = async (itemData: NewItemData): Promise<ItemData> => {
+    try {
+        const response = await AxiosInstance.post('item/', itemData);
+        return response.data;
+    } catch (error) {
+        console.error('Error adding item:', error);
+        throw error;
+    }
+};
+
+export const deleteItem = async (id: number): Promise<void> => {
+    try {
+        await AxiosInstance.delete(`item/${id}/`);
+        console.log(`Item with id ${id} deleted successfully.`);
+    } catch (error) {
+        console.error(`Error deleting item with id ${id}:`, error);
+        throw error;
+    }
+};
+
+export const updateItem = async (id: number, itemData: NewItemData): Promise<ItemData> => {
+    try {
+        const response = await AxiosInstance.patch(`item/${id}/`, itemData);
+        return response.data;
+    } catch (error) {
+        console.error(`Error updating item with id ${id}:`, error);
+        throw error;
+    }
+};
+
+
