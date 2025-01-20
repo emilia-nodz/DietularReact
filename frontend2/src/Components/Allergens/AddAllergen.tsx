116c2e30b4a40ade37fdc186aa563ba073bde91a
TamaTamaTamaTamaTama <domizabiel12@gmail.com>
2025-01-20 17:58:16 +0100

    Wstępna implementacja metod get oraz post, tylko na allergenie

--- frontend2/src/Components/Allergens/AddAllergen.tsx
+++ frontend2/src/Components/Allergens/AddAllergen.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../Styles/Form.css"
-
+import {AllergenData, addAllergen } from "../../Services/AllergenService";
+ 
 const AddAllergen = () => {
+    const [NewAllergenName, setAllergenName] = useState<string>('');
+    const [allergens, setAllergens] = useState<AllergenData[]>([]); 
+
+  const HandlePost = async () => {
+        if(NewAllergenName.length>0) {
+            try {
+                const newAllergen = await addAllergen({name: NewAllergenName})
+                setAllergens([...allergens, newAllergen]);
+                setAllergenName('');
+            }   catch (error) {
+                console.error('Error adding allergen:', error);
+            }
+        } else {
+            console.log('Please provide a valid allergen name.');
+        }
+    };
+ 
+    const HandleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAllergenName(event.target.value);
+      }
+   
+   
     return (
         <>
             <div className="main-container">
             <h1>Add allergen</h1>
+            
+            <input 
+                name="NewAllergenNameInput" 
+                value={NewAllergenName} 
+                onChange={HandleInputChange} 
+            />
+             <button onClick={HandlePost}>Confirm</button>
+
             {/* <div className="form-container">
                 <form [formGroup]="formModel" (ngSubmit)="submitForm()">
                     <div className="form-thing">
