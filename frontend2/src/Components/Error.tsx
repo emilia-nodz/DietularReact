00220116a4d1590b5c75ac9bab6e7e09bf7dbbcd
emilia-nodz <extra.maslo.game@gmail.com>
2025-02-02 16:49:25 +0100

    Rozpoczęcie walidacji

---
+++ frontend2/src/Components/Error.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Error = (props: { status: any; info: string }) => {
+	return (
+	   <div>
+		{!props.status && (
+            <p style={{ color: "#FF7F7F", fontWeight: "bold" }}>
+                {props.info}
+            </p>
+        )}  
+	   </div>
+	);
+};
+ 
+export default Error;
